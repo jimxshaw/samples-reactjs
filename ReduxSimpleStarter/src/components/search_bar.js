ec31b5c32c16f28af13e6ba4dc058cec09e7262f
Jim Shaw <jimxshaw@gmail.com>
2016-02-14 13:48:19 -0600

    refactored SearchBar component class with fat arrow notation for terseness

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -12,10 +12,24 @@ import React, {Component} from "react";
 // Here's an example of a class component. Define a new class called SearchBar that 
 // extends or inherits the members of React.Component. Every class must have a render 
 // method. Note that if the above import does not explicitly pull in {Component} then 
-// you must fully qualify the below class by writing extends React.Component.
+// you must fully qualify the below class by writing extends React.Component. Render 
+// method must also always return some JSX or errors will occur. 
 class SearchBar extends Component {
 	render() {
-		return <input />;
+		return <input onChange={this.onInputChange} />;
+		// // Arrow notation can be used to increase code terseness, albeit at the
+		// // the expense of readibility. Comment out onInputChange() {} and the
+		// // above first return statement. Comment in the below second return.
+		// // The app should still work the same as before.
+		// return <input onChange={event => console.log(event.target.value)} />;
+	}
+
+	// Whenever the input element changes, run the code inside. This is essentially 
+	// an event handler that's placed in the returned JSX of render. The event handler 
+	// will trigger whenever an event occurs, specifically the change event hence 
+	// why we wrote onChange above.
+	onInputChange(event) {
+		console.log(event.target.value);
 	}
 }
 
