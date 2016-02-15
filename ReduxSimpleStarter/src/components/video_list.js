5557b7d79f8a1c759acbaece39d08607127b9658
Jim Shaw <jimxshaw@gmail.com>
2016-02-15 12:08:37 -0600

    created functional component video_list_item.js. incorporated it with the VideoList component and tested it.

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -1,8 +1,9 @@
-// The video list component doesn't utilize state. 
+// The VideoList component doesn't utilize state. 
 // It doesn't have any user interaction or re-renders itself. 
 // So it remains a functional component. 
 
 import React from "react";
+import VideoListItem from "./video_list_item";
 
 // Props are class component state properties being pass in to
 // this functional class. For example, VideoList's parent component 
@@ -14,9 +15,20 @@ import React from "react";
 // In class components, props are available in any method we define
 // and can be used as this.props. 
 const VideoList = (props) => {
+	// Fat arrow notation is used with the usual .map method for conciseness.
+	// For every video object in the passed in props videos array from App component,
+	// return a VideoListItem component and assign said video object to the
+	// component's video attribute. After map finishes, assign the result to a 
+	// constant called videoItems.
+	const videoItems = props.videos.map((video) => {
+		return <VideoListItem video={video} />;
+	});
+
+	// React is smart enough to know that videoItems is an array and will try to 
+	// display each item within the ul.
 	return (
 		<ul className="col-md-4 list-group">
-			{props.videos.length}
+			{videoItems}
 		</ul>
 	);
 };
---
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -0,0 +1,19 @@
+// VideoListItem will comprise and render 
+// on the VideoList component. 
+
+import React from "react";
+
+const VideoListItem = (props) => {
+	return <li>Video</li>;
+};
+
+export default VideoListItem;
+
+
+
+
+
+
+
+
+
