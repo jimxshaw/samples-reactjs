31cdebd725fee3e3b494873779b25372d59371af
Jim Shaw <jimxshaw@gmail.com>
2016-02-22 18:25:24 -0600

    added onSubmit form handler to SearchBar component

--- WeatherApp/src/containers/search_bar.js
+++ WeatherApp/src/containers/search_bar.js
@@ -19,13 +19,19 @@ export default class SearchBar extends Component {
   }
 
   onInputChange(event) {
-    console.log(event.target.value);
     this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event) {
+    // Don't submit the form immediately because we haven't retrieved weather data yet.
+    event.preventDefault();
+
+    // We need to go and fetch weather data. 
   } 
 
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input 
           placeholder="Get a five day forcast in your favorite cities"
           className="form-control"
