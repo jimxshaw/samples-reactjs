24806d3d4b5112c5ead0e754aab16245ec1fce03
Jim Shaw <jimxshaw@gmail.com>
2016-02-24 18:18:45 -0600

    npm installed redux-promise middleware, imported it and inserted it into applyMiddleware function in src/index.js

--- WeatherApp/src/index.js
+++ WeatherApp/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import ReduxPromise from "redux-promise";
+
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Imported ReduxPromise middleware and inserted it into our applyMiddleware function. 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
