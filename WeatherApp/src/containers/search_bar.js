cf62c667854288a2f150e76c3b93b904e8bce78f
Jim Shaw <jimxshaw@gmail.com>
2016-02-26 16:57:32 -0600

    refactored SearchBar component into a container that uses the fetcherWeather action creator

--- WeatherApp/src/containers/search_bar.js
+++ WeatherApp/src/containers/search_bar.js
@@ -1,4 +1,11 @@
+// SearchBar is a container that uses the fetchWeather action creator 
+// in actions/index.js.
+
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {fetchWeather} from "../actions/index";
+
 
 export default class SearchBar extends Component {
   constructor(props) {
@@ -14,8 +21,9 @@ export default class SearchBar extends Component {
 
     // General rule for the keyword this. If we're ever passing around a callback 
     // as a function and that callback has a reference to keyword this, the 
-    // context must be bound. 
+    // context must be bound. Again, we're binding this again with this.onFormSubmit.
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -26,7 +34,12 @@ export default class SearchBar extends Component {
     // Don't submit the form immediately because we haven't retrieved weather data yet.
     event.preventDefault();
 
-    // We need to go and fetch weather data. 
+    // We need to go and fetch weather data. After importing fetchWeather action creator 
+    // and binding it to props, we can call it here with the user input city. That would 
+    // be the term of this.state. After searching and grasping the city term, we clear 
+    // the term so that a new search could start. 
+    this.props.fetchWeather(this.state.term); 
+    this.setState({term: ""});
   } 
 
   render() {
@@ -45,9 +58,14 @@ export default class SearchBar extends Component {
   }
 }
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
 
-
-
+// connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
+// Since we're not mapping any state to props with this SearchBar container, 
+// we pass in a null value in the first argument. 
+export default connect(null, mapDispatchToProps)(SearchBar);
 
 
 
