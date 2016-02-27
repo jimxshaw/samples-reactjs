11776e59499896360aa53c991d50a7c95766fcad
Jim Shaw <jimxshaw@gmail.com>
2016-02-26 22:44:50 -0600

    added switch statement to reducer_weather.js to capture the action's payload

--- WeatherApp/src/reducers/reducer_weather.js
+++ WeatherApp/src/reducers/reducer_weather.js
@@ -1,3 +1,5 @@
+import {FETCH_WEATHER} from "../actions/index";
+
 // Be aware that the action passed in to this reducer is NOT the 
 // original payload from the action creator fetcherWeather. The 
 // fetchWeather action creator has axios.get that returns a promise and 
@@ -12,8 +14,23 @@
 // redux-promise middleware will simply let the action pass through to 
 // the reducers automatically. It's akin to what happens to actions if 
 // redux-promise middleware was never added to a project in the first place.   
-export default function(state = null, action) {
-  
+export default function(state = [], action) {
+  // The initial state is an array so that we can store weather data for 
+  // multiple cities. 
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // We only grab the data property of the payload because it contains 
+      // all the weather info we need. Other properties such as payload.headers 
+      // or payload.status etc. are not necessary for the app we're building.
+      // Since we're holding multiple cities in an array, why aren't we using 
+      // state.push(action.payload.data) instead? The reason is we NEVER want 
+      // to mutate the state. We ALWAYS want to return a new state to replace 
+      // the old state. If push is used, that's state mutation and will cause 
+      // trouble while concat wipes away the old state and returns a new state 
+      // with the new city data added. This a MAJOR TRAP. Do not ever mutate state.  
+      return state.concat([action.payload.data]);
+      // return [action.payload.data, ...state]; // This is the ES6 equivalent of the above line.
+  }
 
   return state;
 }
