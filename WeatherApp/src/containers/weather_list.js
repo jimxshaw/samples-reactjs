a923f9c62090c9033a2afac9296d251ac9dbb883
Jim Shaw <jimxshaw@gmail.com>
2016-02-27 14:28:47 -0600

    installed react-sparklines and imported it to WeatherList container. rendered sparklines within returned JSX

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import {Sparklines, SparklinesLine} from "react-sparklines";
 
 // WeatherList has to be a container instead of a component because it needs 
 // access to the redux application state. SearchBar captures the user city 
@@ -16,10 +17,17 @@ class WeatherList extends Component {
   // as the key, since every city is unique.
   renderWeather(cityData) {
     const cityName = cityData.city.name;
+    // Grab our list of temperatures in kelvin and assign it to an array called temps.
+    const temps = cityData.list.map(weather => weather.main.temp);
 
     return (
       <tr key={cityName}>
         <td>{cityName}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
