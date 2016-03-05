0d2f8fb22c03bb6028689038ce32d39be92e137c
Jim Shaw <jimxshaw@gmail.com>
2016-03-05 13:41:18 -0600

    created boilerplate posts_index.js component. incorporated it in routes.js with react-router's IndexRoute object

--- ReduxBlog/src/routes.js
+++ ReduxBlog/src/routes.js
@@ -2,12 +2,11 @@
 import React from "react";
 
 // Route is the object that defines a match between a url and a component.
+// IndexRoute is a helper that behaves like a route but will be shown whenever the url 
+// matches up with a path defined by the parent but not one of the children. 
 import {Route, IndexRoute} from "react-router";
 import App from "./components/app";
-
-const Greeting = () => {
-  return <div>"Hello!"</div>
-};
+import PostsIndex from "./components/posts_index";
 
 // Route JSX tag is where we house our paths and components that we want to share.
 // This maps the route of / to the component of App. Whenever users are at /, show the
@@ -18,11 +17,13 @@ const Greeting = () => {
 // this.props.children. In order for child components to render within the parent 
 // component, the line {this.props.children} must be added within the parent component's
 // returned JSX. 
+// The IndexRoute JSX tag does not need a path. By using IndexRoute, if the route is / 
+// show App and PostsIndex because / is the parent. If the route is / plus a path with 
+// a regular <Route> tag then simply show App and that path's connected component, 
+// IndexRoute will be ignored in this instance. 
 export default ( 
   <Route path="/" component={App}>
-    <Route path="greet" component={Greeting} />
-    <Route path="greet2" component={Greeting} />
-    <Route path="greet3" component={Greeting} />
+    <IndexRoute component={PostsIndex} />
   </Route>
 );
 
