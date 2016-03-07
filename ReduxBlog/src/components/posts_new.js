08df233ce3acccaa24e5cfb476754571538f068e
Jim Shaw <jimxshaw@gmail.com>
2016-03-07 14:56:55 -0600

    imported formReducer from redux-form, added it to our rootReducer. imported reduxForm function from redux-form and used the function in our posts_new.js component. created PostsNew form in jsx

--- ReduxBlog/src/components/posts_new.js
+++ ReduxBlog/src/components/posts_new.js
@@ -1,19 +1,68 @@
 // This is the component to create a new post at 
 // route /posts/new.
 import React, {Component} from "react";
+// This imported reduxForm is nearly identical to the connect function 
+// from react-redux.
+import {reduxForm} from "redux-form";
 
+// PostsNew uses redux-form. How this component works with that package 
+// is this. PostsNew tells react-form to keep track of three fields(inputs):
+// title, categories, content. Next, react-form takes the instructions and 
+// places its own set of rules to each field so that the fields will know 
+// what to do when they're updated. React-form also takes charge of the 
+// PostsNew form submission functionality. 
 class PostsNew extends Component {
   render() {
     return (
-      <div>Create Form</div>
+      <form>
+        <h3>Create a New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// PostsNew form configuration is placed here, letting redux-form know 
+// the fields that it will take charge. First, we name our form PostsNew. 
+// The form name does NOT have to be named the same as the component name. 
+// Next is an array containing our fields that will be on the form.
+export default reduxForm({
+  form: "PostsNew",
+  fields: ["title", "categories", "content"]
+})(PostsNew);
 
 
+/*
+Behind the scenes, when users type something in PostsNew form..... the changes 
+will be tracked on our application state. It will look something like:
+state === {
+  form: {
+    PostsNew: {
+      title: "user input title",
+      categories: "user input categories",
+      content: "user input content"
+    }
+  }
+};
 
+Note that the form key within state is the form key within our rootReducer in
+reducers/index.js.
+*/
 
 
 
--- ReduxBlog/src/reducers/index.js
+++ ReduxBlog/src/reducers/index.js
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
 import PostsReducer from "./reducer_posts";
+// Import redux-form, grab the reducer object from it and alias 
+// it as formReducer. Aliasing is used as a convention to avoid 
+// naming conflicts in case other kinds of reducers are imported.
+import {reducer as formReducer} from "redux-form";
 
+// Using the form key for our formReducer is vital. It has to be 
+// named form. 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
