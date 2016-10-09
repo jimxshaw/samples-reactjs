96d11955ca2acd906cbbe64b41adae38d8adefb5
Jim Shaw <jimxshaw@gmail.com>
2016-10-09 14:09:46 -0500

    Added boilerplate index.html. Added gulpfile.

---
+++ PSAdmin/gulpfile.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var gulp = require("gulp");
+// Run a local dev server.
+var connect = require("gulp-connect");
+// Open a URL in a web browser.
+var open = require("gulp-open");
+
+var config = {
+    port: 9005,
+    devBaseUrl: "http://localhost",
+    paths: {
+        html: "./src/*.html",
+        dist: "./dist"
+    }
+};
+
+// Start a local dev server.
+gulp.task("connect", function () {
+    connect.server({
+        root: ["dist"],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+});
+
+// Open task depends on the connect task. So we do the 
+// connect task dependency first and do this task.
+// Go get index.html and the open it in the browser at a 
+// specific url, which is the url we defined above. It'll be 
+// opened at localhost port 9005.
+gulp.task("open", ["connect"], function () {
+    gulp.src("dist/index.html")
+        .pipe(open({ uri: config.devBaseUrl + ":" + config.port + "/" }));
+});
+
+// Have gulp get our html files, put them in dist folder and perform some task. 
+// In this particular task, we want gulp to reload our dev server. 
+gulp.task("html", function () {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+// This is a default task we want to run. When we goto the command line and 
+// issue the gulp command, gulp will run the open task and the html task.
+gulp.task("default", ["html", "open"]);
+
+
