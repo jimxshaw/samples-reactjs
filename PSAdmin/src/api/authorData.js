76d8cb808b07240cf6951acaafa11dec254f65c7
Jim Shaw <jimxshaw@gmail.com>
2016-10-13 21:42:46 -0500

    Added mock api data and module. Added lodash.

---
+++ PSAdmin/src/api/authorData.js
@@ -0,0 +1,21 @@
+// authorData.js
+
+module.exports = {
+    authors: [
+        {
+            id: 'james-madison',
+            firstName: 'James',
+            lastName: 'Madison'
+        },
+        {
+            id: 'thomas-jefferson',
+            firstName: 'Thomas',
+            lastName: 'Jefferson'
+        },
+        {
+            id: 'john-adams',
+            firstName: 'John',
+            lastName: 'Adams'
+        }
+    ]
+};
