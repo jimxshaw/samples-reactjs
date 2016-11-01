481a6610ec0aa887414b7af06fc8d0ed3b38d335
Jim Shaw <jimxshaw@gmail.com>
2016-10-31 21:16:33 -0500

    Utilized react router.

---
+++ PSAdmin/src/components/app.js
@@ -0,0 +1,33 @@
+// app.js
+
+// Disabling check because we can't run strict mode as
+// we need global variables.
+
+/* eslint-disable strict */
+
+var React = require("react");
+var Header = require("./common/header");
+var RouteHandler = require("react-router").RouteHandler;
+
+$ = jQuery = require("jquery");
+
+var App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Header />
+                <div className="container-fluid">
+                    <RouteHandler />
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = App;
+
+
+
+
+
+
