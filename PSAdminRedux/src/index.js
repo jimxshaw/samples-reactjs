8f6215a72172f12938ab80eaf01c516cc1c6abe7
Jim Shaw <jimxshaw@gmail.com>
2016-12-23 13:34:44 -0600

    Connected our store to our react app by using Provider higher order component from react-redux.

--- PSAdminRedux/src/index.js
+++ PSAdminRedux/src/index.js
@@ -1,12 +1,20 @@
 import "babel-polyfill";
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 import { Router, browserHistory } from "react-router";
+import configureStore from "./store/configureStore";
 import routes from "./routes";
 import "./styles/styles.css"; // Webpack can import CSS files.
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+const store = configureStore();
+
+// The higher order Provider component essentially wraps our
+// entire application so that our react app is connected to our store.
 render(
-  <Router history={ browserHistory } routes={ routes }/>,
+  <Provider store={ store }>
+    <Router history={ browserHistory } routes={ routes }/>
+  </Provider>,
   document.getElementById("app")
 );
