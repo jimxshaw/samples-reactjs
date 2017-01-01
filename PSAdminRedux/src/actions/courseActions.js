714d54e1d7e8121482702cd7d9584616c501aef1
Jim Shaw <jimxshaw@gmail.com>
2017-01-01 15:11:23 -0600

    Added loadCourses thunk function that hits our mock courses api. Updated actionTypes accordingly.

--- PSAdminRedux/src/actions/courseActions.js
+++ PSAdminRedux/src/actions/courseActions.js
@@ -1,9 +1,20 @@
 import * as types from "./actionTypes";
+import courseApi from "../api/mockCourseApi";
 
-export function createCourse(course) {
+export function loadCoursesSuccess(courses) {
   //debugger;
   return {
-    type: types.CREATE_COURSE,
-    course: course
+    type: types.LOAD_COURSES_SUCCESS,
+    course: courses
+  };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
   };
 }
