db2b72aaf445750e8bb54e6d53e449cc41692d84
Jim Shaw <jimxshaw@gmail.com>
2019-03-16 16:09:48 -0500

    Update first component.

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,18 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
-  return <div>Hello World!</div>;
+  const buttonText = { text: "Click here" };
+  const style = { backgroundColor: "blue", color: "white" };
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>{buttonText.text}</button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
