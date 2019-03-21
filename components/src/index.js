2da519688475d9eea1122a1d08186a86859da4fc
Jim Shaw <jimxshaw@gmail.com>
2019-03-21 17:51:19 -0500

    Add CommentDetail component.

--- components/src/index.js
+++ components/src/index.js
@@ -1,52 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Jamie
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Yesterday at 11:10AM</span>
-          </div>
-          <div className="text">Who's this?</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Chris
-          </a>
-          <div className="metadata">
-            <span className="date">January 23 at 3:43PM</span>
-          </div>
-          <div className="text">Cool beans.</div>
-        </div>
-      </div>
+      <CommentDetail />
     </div>
   );
 };
