b96d20f4998c24e8d810fa70f54bb851c6eb8bb8
Jim Shaw <jimxshaw@gmail.com>
2019-03-27 22:04:50 -0500

    Update errorMessage state.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,20 +6,28 @@ class App extends React.Component {
     super(props);
 
     // Only directly assign state in the constructor.
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         // State can only be updated using setState.
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React requires the render method.
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
