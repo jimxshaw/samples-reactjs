b7fd7309b094d66659478960063f8d18b2c6ac52
Jim Shaw <jimxshaw@gmail.com>
2019-05-07 10:56:47 -0500

    Add promise on to axios get request.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,15 +4,19 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: {
-        query: term
-      },
-      headers: {
-        Authorization:
-          "Client-ID 3c8209efcef6b7a184e8f40b3ba0df5c9b9c749ba384c9cdd45e542ec7e8ef82"
-      }
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: {
+          query: term
+        },
+        headers: {
+          Authorization:
+            "Client-ID 3c8209efcef6b7a184e8f40b3ba0df5c9b9c749ba384c9cdd45e542ec7e8ef82"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
