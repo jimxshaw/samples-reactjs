90a66ef5b4ec63e0e5618ddbea645bd469a49133
Jim Shaw <jimxshaw@gmail.com>
2019-05-07 10:46:42 -0500

    Add axios get request to App component's onSearchSubmit.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID 3c8209efcef6b7a184e8f40b3ba0df5c9b9c749ba384c9cdd45e542ec7e8ef82"
+      }
+    });
   }
 
   render() {
