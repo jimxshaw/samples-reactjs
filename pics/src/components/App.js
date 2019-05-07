6f59d9d5586792bc3d35c705af57945890f55aca
Jim Shaw <jimxshaw@gmail.com>
2019-05-07 11:03:48 -0500

    Refactor axios promise to async await.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,20 +3,18 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: {
-          query: term
-        },
-        headers: {
-          Authorization:
-            "Client-ID 3c8209efcef6b7a184e8f40b3ba0df5c9b9c749ba384c9cdd45e542ec7e8ef82"
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID 3c8209efcef6b7a184e8f40b3ba0df5c9b9c749ba384c9cdd45e542ec7e8ef82"
+      }
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
