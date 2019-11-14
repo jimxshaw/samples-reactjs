c0bc6a96bf4a38ba354c1798795dc2a17de67eba
Jim Shaw <jimxshaw@gmail.com>
2019-11-13 21:33:03 -0600

    Implement onTermSubmit by issuing axios GET request.

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
   onTermSubmit = term => {
-    console.log(term);
+    youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
   };
 
   render() {
     return (
-      <div class="ui container">
+      <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
