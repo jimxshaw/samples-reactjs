41cbd767221f72deb2c2273d29577bc0402416be
Jim Shaw <jimxshaw@gmail.com>
2019-11-13 21:11:15 -0600

    Add boilerplate configuration of axios.

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyD7k7azBgZaqszylMM7MrP0INPCwmYYIKU";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
