2d1d37a63b628b73ce80a16c6fb59a5d4c405b09
Jim Shaw <jimxshaw@gmail.com>
2021-07-07 13:28:30 -0500

    Add items array.

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a JavaScript frontend library'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is popular among engineers'
+  },
+  {
+    title: 'How is React used?',
+    content: 'React involves creating components'
+  }
+];
+
 export default () => {
   return (
     <div>
