5e1fe87d70cacef779be0c65e2fa96a5a4c4d832
Jim Shaw <jimxshaw@gmail.com>
2021-07-07 13:40:31 -0500

    Refactor Accordion to use React.Fragment to remove extra div inserted by Semantic UI css.

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Accordion = ({items}) => {
   const renderedItems = items.map(item => {
     return (
-    <div key={item.title}>
+    <React.Fragment key={item.title}>
       <div className="title active">
         <i className="dropdown icon" />
         {item.title}
@@ -13,7 +13,7 @@ const Accordion = ({items}) => {
           {item.content}
         </p>
       </div>
-    </div>);
+    </React.Fragment>);
   });
 
   return <div className="ui styled accordion">{renderedItems}</div>;
