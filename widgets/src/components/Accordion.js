b803dca5d424f36c347cb27aaaf2af15d21f935d
Jim Shaw <jimxshaw@gmail.com>
2021-07-07 21:47:31 -0500

    Add comments.

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,6 +1,13 @@
 import React, {useState} from 'react';
 
 const Accordion = ({items}) => {
+  // Array destructuring: we're getting back 2 elements when
+  // calling useState. The first element is the piece of variable
+  // that will change over time. The second element is a function
+  // we call to update a piece of state. Calling the second element
+  // will automatically cause the component to re-render.
+  // useState takes in a default value argument for 
+  // that piece of state.
   const [activeIndex, setActiveIndex] = useState(null);
 
   const onTitleClick = (index) => {
