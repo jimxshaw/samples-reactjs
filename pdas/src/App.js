7eca690b762fbfcf2746d7dc7cd1512a1081a5c9
Jim Shaw <jimxshaw@gmail.com>
2023-06-30 18:19:34 -0500

    Update App with description in ProfileCard.

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -22,15 +22,30 @@ function App() {
         <section className="section">
           <div className="columns">
             <div className="column is-4">
-              <ProfileCard title="Alexa" handle="@alexa2" image={AlexaImage} />
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa2"
+                image={AlexaImage}
+                description="Alexa was created by Amazon."
+              />
             </div>
 
             <div className="column is-4">
-              <ProfileCard title="Cortana" handle="@cortana84" image={CortanaImage} />
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana84"
+                image={CortanaImage}
+                description="Cortana was created by Microsoft."
+              />
             </div>
 
             <div className="column is-4">
-              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was created by Apple."
+              />
             </div>
           </div>
         </section>
