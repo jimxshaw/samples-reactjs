5a939bfd3eef6e3bbbcdf1ba0de1b6492237f230
Jim Shaw <jimxshaw@gmail.com>
2023-07-01 20:05:45 -0500

    Update AnimalShow to update heart state.

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -5,6 +5,7 @@ import dog from './svg/dog.svg';
 import gator from './svg/gator.svg';
 import heart from './svg/heart.svg';
 import horse from './svg/horse.svg';
+import { useState } from 'react';
 
 // When mapping an object if the key & value are exactly the same then
 // a JS shortcut is to just have one name represent both key & value.
@@ -18,9 +19,20 @@ const svgMap = {
 };
 
 function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <img alt="animal" src={svgMap[type]} />
+      <img
+        alt="heart"
+        src={heart}
+        style={{ width: 10 + 10 * clicks + 'px' }}
+      />
     </div>
   );
 }
