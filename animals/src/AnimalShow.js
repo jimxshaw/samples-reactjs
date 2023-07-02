ce7758e3d6f27946469a4f65a74737e9c6f023c3
Jim Shaw <jimxshaw@gmail.com>
2023-07-01 19:41:20 -0500

    Update AnimalShow to display svg files.

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,6 +1,27 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+// When mapping an object if the key & value are exactly the same then
+// a JS shortcut is to just have one name represent both key & value.
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse
+};
+
 function AnimalShow({ type }) {
   return (
-    <div>{type}</div>
+    <div>
+      <img alt="animal" src={svgMap[type]} />
+    </div>
   );
 }
 
