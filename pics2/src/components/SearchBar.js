bec57e20b2a0206adfdbbcc478eb1ec8c2eb1693
Jim Shaw <jimxshaw@gmail.com>
2023-07-03 18:13:33 -0500

    Update SearchBar input with state.

--- pics2/src/components/SearchBar.js
+++ pics2/src/components/SearchBar.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState('');
+
+  // The event always exists and automatically be 
+  // passed into an event handler that is assigned
+  // to a plain HTML element, such as form, input etc.
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    onSubmit('dogs');
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
   };
 
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input />
+        <input value={term} onChange={handleChange} />
       </form>
     </div>
   );
