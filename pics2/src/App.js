0d82ca0fa2f7f79bee5365ae36bf79d950555e21
Jim Shaw <jimxshaw@gmail.com>
2023-07-03 19:28:03 -0500

    Call searchImages from api file.

--- pics2/src/App.js
+++ pics2/src/App.js
@@ -1,8 +1,9 @@
 import SearchBar from './components/SearchBar';
+import searchImages from './api';
 
 function App() {
   const handleSubmit = (term) => {
-    console.log('Searching for', term);
+    searchImages(term);
   };
 
   return (
