b81ddbdc1c214b94b788bbb09f1a5af5fc0c83d1
Jim Shaw <jimxshaw@gmail.com>
2023-07-05 14:27:54 -0500

    Update App.

--- todo-basic/src/App.js
+++ todo-basic/src/App.js
@@ -5,16 +5,18 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const createTodo = (todo) => {
-    console.log(todo);
-    setTodos(todo);
+    // Array and object state updates must be done in
+    // a particular way or will cause trouble later on.
+    // https://state-updates.vercel.app/
+    const updatedTodos = [...todos, { id: 123, description: todo }];
+
+    setTodos(updatedTodos);
   };
 
   return (
     <div>
+      {todos.length}
       <TodoCreate onCreate={createTodo} />
-      <div>
-        {todos}
-      </div>
     </div>
   );
 }
