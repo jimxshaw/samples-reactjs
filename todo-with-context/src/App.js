6056725ab03535769428c624bbfe0f89e35f7d19
Jim Shaw <jimxshaw@gmail.com>
2023-07-07 14:08:59 -0500

    Refactor App component to use context custom Provider.

--- todo-with-context/src/App.js
+++ todo-with-context/src/App.js
@@ -1,17 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import TodoCreate from './components/TodoCreate';
 import TodoList from './components/TodoList';
-import axios from 'axios';
+import TodosContext from './context/todos';
 
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const getTodos = async () => {
-    const response = await axios.get('http://localhost:3001/todos');
-
-    setTodos(response.data)
-  }
+  const { getTodos } = useContext(TodosContext);
 
   // The arrow function will always be called right after the initial render.
   // The 2nd argument triggers whether the arrow function will be called in
@@ -23,55 +17,11 @@ function App() {
     getTodos();
   }, []);
 
-  const createTodo = async (todo) => {
-    const response = await axios.post('http://localhost:3001/todos', {
-      description: todo
-    });
-
-    // Array and object state updates must be done in
-    // a particular way or will cause trouble later on.
-    // https://state-updates.vercel.app/
-    const updatedTodos = [...todos, response.data];
-
-    setTodos(updatedTodos);
-  };
-
-  const editTodoById = async (id, newDescription) => {
-    const response = await axios.put(`http://localhost:3001/todos/${id}`, {
-      description: newDescription
-    });
-
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        // Take all the response data key-value pairs and add it to the new object.
-        return { ...todo, ...response.data };
-      }
-
-      return todo;
-    });
-
-    setTodos(updatedTodos);
-  };
-
-  const deleteTodoById = async (id) => {
-    await axios.delete(`http://localhost:3001/todos/${id}`);
-
-    // Filter does not modify existing array but 
-    // creates a new one and copies contents over.
-    const updatedTodos = todos.filter((todo) => {
-      return todo.id !== id;
-    });
-
-    setTodos(updatedTodos);
-  }
-
   return (
     <div className="app">
       <h1>To Do List</h1>
-      <TodoList todos={todos}
-        onDelete={deleteTodoById}
-        onEdit={editTodoById} />
-      <TodoCreate onCreate={createTodo} />
+      <TodoList />
+      <TodoCreate />
     </div>
   );
 }
