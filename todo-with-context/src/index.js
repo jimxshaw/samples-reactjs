dbaf48a563abe8bff8862f0cd09cc808dd663eaa
Jim Shaw <jimxshaw@gmail.com>
2023-07-07 12:46:23 -0500

    In index js, wrap context provider around App component.

--- todo-with-context/src/index.js
+++ todo-with-context/src/index.js
@@ -2,8 +2,13 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TodosContext from './context/todos';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <TodosContext.Provider value={5}>
+    <App />
+  </TodosContext.Provider>
+);
