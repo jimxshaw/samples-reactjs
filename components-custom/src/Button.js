5866ab48c69253e8959b1179ddcdf741750a2331
Jim Shaw <jimxshaw@gmail.com>
2023-07-09 10:42:20 -0500

    Add PropType rules to Button component.

--- components-custom/src/Button.js
+++ components-custom/src/Button.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 // The prop children is a React keyword.
 function Button({
   children,
@@ -13,5 +15,29 @@ function Button({
     <button>{children}</button>
   );
 }
+
+Button.propTypes = {
+  checkVariationValue: ({
+    primary,
+    secondary,
+    success,
+    warning,
+    danger
+  }) => {
+    // Button component should only ever have one of these props.
+    // Covert the prop values into numbers and if they > 1 then
+    // trigger this validation rule.
+    // Possible values are true, false, undefined (if not included).
+    const count = Number(!!primary)
+      + Number(!!secondary)
+      + Number(!!success)
+      + Number(!!warning)
+      + Number(!!danger);
+
+    if (count > 1) {
+      return new Error('Only one of primary, second, success, warning or danger can be true');
+    }
+  }
+};
 
 export default Button;
