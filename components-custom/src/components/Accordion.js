5da384882bf04ffd2a4a35eefa97f4d6cb63c779
Jim Shaw <jimxshaw@gmail.com>
2023-07-11 11:12:55 -0500

    Update Accordion component.

--- components-custom/src/components/Accordion.js
+++ components-custom/src/components/Accordion.js
@@ -1,7 +1,20 @@
-function Accordion() {
+function Accordion({ items }) {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.id}>
+        <div>
+          {item.label}
+        </div>
+        <div>
+          {item.content}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
-      Accordion!
+      {renderedItems}
     </div>
   );
 }
