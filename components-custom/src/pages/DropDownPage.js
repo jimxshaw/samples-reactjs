85242c4adbe404cc5ee926c3a1e94e2d89dabfba
Jim Shaw <jimxshaw@gmail.com>
2023-07-14 16:57:59 -0500

    Refactor DropDown into its own page.

---
+++ components-custom/src/pages/DropDownPage.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import DropDown from "../components/DropDown";
+
+function DropDownPage() {
+  // By default nothing should be selected in the drop down.
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: 'Green', value: 'green' },
+    { label: 'Purple', value: 'purple' },
+    { label: 'Yellow', value: 'yellow' },
+  ];
+
+  return (
+    <div className="flex">
+      {/* Naming convention says form control prop names should be value and onChange. */}
+      <DropDown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
+}
+
+export default DropDownPage;
