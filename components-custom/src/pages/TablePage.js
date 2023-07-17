b0e02046636463fc296297982e4f8af3745eef45
Jim Shaw <jimxshaw@gmail.com>
2023-07-17 18:03:31 -0500

    Add mock data to TablePage.

--- components-custom/src/pages/TablePage.js
+++ components-custom/src/pages/TablePage.js
@@ -1,9 +1,17 @@
 import Table from "../components/Table";
 
 function TablePage() {
+  const data = [
+    { name: 'Apple', color: 'bg-red-500', score: 5 },
+    { name: 'Orange', color: 'bg-orange-500', score: 3 },
+    { name: 'Banana', color: 'bg-yellow-500', score: 4 },
+    { name: 'Grape', color: 'bg-purple-500', score: 7 },
+    { name: 'Pear', color: 'bg-green-500', score: 9 }
+  ];
+
   return (
     <div>
-      <Table />
+      <Table data={data} />
     </div>
   );
 }
