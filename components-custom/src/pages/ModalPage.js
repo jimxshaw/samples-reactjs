d4f04925da002837d2a180f0840f36ffafe89f76
Jim Shaw <jimxshaw@gmail.com>
2023-07-17 16:40:12 -0500

    Add button to ModalPage.

--- components-custom/src/pages/ModalPage.js
+++ components-custom/src/pages/ModalPage.js
@@ -1,8 +1,10 @@
 import Modal from "../components/Modal";
+import Button from "../components/Button";
 
 function ModalPage() {
   return (
     <div>
+      <Button primary>Open Modal</Button>
       <Modal />
     </div>
   );
