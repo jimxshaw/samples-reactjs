7dddb4305790c5d05eab19e194dea9362acb836d
Jim Shaw <jimxshaw@gmail.com>
2023-07-17 17:39:23 -0500

    Add styling to Modal.

--- components-custom/src/components/Modal.js
+++ components-custom/src/components/Modal.js
@@ -7,8 +7,12 @@ function Modal({ onClose, children, actionBar }) {
       <div className="absolute inset-0 bg-gray-300 opacity-80" onClick={onClose}></div>
       {/* Actual Modal */}
       <div className="absolute inset-40 p-10 bg-white">
-        {children}
-        {actionBar}
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">
+            {actionBar}
+          </div>
+        </div>
       </div>
     </div>,
     document.querySelector('.modal-container')
