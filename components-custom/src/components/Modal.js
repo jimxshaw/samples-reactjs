9dc5de5482427e08fdeb84e17b141c08856a8e8d
Jim Shaw <jimxshaw@gmail.com>
2023-07-17 16:50:15 -0500

    Update Modal with boilerplate.

--- components-custom/src/components/Modal.js
+++ components-custom/src/components/Modal.js
@@ -1,6 +1,13 @@
 function Modal() {
   return (
-    <div>Modal!</div>
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80">
+
+      </div>
+      <div className="absolute insert-40 p-10 bg-white">
+        This is the Modal
+      </div>
+    </div>
   );
 }
 
