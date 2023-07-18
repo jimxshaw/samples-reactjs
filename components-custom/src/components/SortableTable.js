7d72c85db8a65cd5d4c1ef864c5e9ee123f4c204
Jim Shaw <jimxshaw@gmail.com>
2023-07-18 18:28:49 -0500

    Update SortableTable with updated config logic.

--- components-custom/src/components/SortableTable.js
+++ components-custom/src/components/SortableTable.js
@@ -1,8 +1,27 @@
 import Table from "./Table";
 
 function SortableTable(props) {
+  // Do not modify config itself. Use the config
+  // as a basis of a new set of objects using map.
+  const { config } = props;
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      // Just return the column if it's not sortable.
+      return column;
+    }
+
+    // Return all the columns but with an added header function.
+    return {
+      ...column,
+      header: () => <th>{column.label} IS SORTABLE</th>
+    };
+  });
+
   return (
-    <Table {...props} />
+    // There's already a config in props but since we're also passing
+    // in updateConfig after, updatedConfig overrides the prior config.
+    <Table {...props} config={updatedConfig} />
   );
 }
 
