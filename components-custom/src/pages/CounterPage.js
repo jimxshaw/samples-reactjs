845dd505ee224f6afd7160c6bff37649a0424f7b
Jim Shaw <jimxshaw@gmail.com>
2023-07-20 14:35:09 -0500

    Refactor CounterPage without custom hook for testing.

--- components-custom/src/pages/CounterPage.js
+++ components-custom/src/pages/CounterPage.js
@@ -1,19 +1,25 @@
-import useCounter from '../hooks/use-counter';
+import { useState } from 'react';
 import Button from '../components/Button';
 
-// General guidelines for creating Custom Hooks:
-// - Find code in a component related to a single piece of state.
-// - Copy paste it all into a helper function.
-// - Fix all the broken references.
-// - That's it, now we have a custom hook.
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
   return (
     <div>
       <h1>Count is {count}</h1>
       <Button onClick={increment}>
         Increment
+      </Button>
+      <Button onClick={decrement}>
+        Decrement
       </Button>
     </div>
   );
