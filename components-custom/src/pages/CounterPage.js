f951d0d5c79e56822819e1cbc09ea9e09ce088f1
Jim Shaw <jimxshaw@gmail.com>
2023-07-21 09:38:03 -0500

    Refactor CounterPage to use useReducer.

--- components-custom/src/pages/CounterPage.js
+++ components-custom/src/pages/CounterPage.js
@@ -1,36 +1,56 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+// Reducer: no async/await, no requests,
+// no promises, no outside variables.
+const reducer = (state, action) => {
+  // Whatever gets returned will be 
+  // the new state!
+  // If nothing if returned then the
+  // new state will be UNDEFINED.
+  // Never directly assign values to state properties.
+  // Instead, always use the update approaches here:
+  // https://state-updates.vercel.app/
+  return {
+    ...state,
+    count: state.count + 1
+  };
+};
+
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0
+  });
 
   const increment = () => {
-    setCount(count + 1);
+    dispatch();
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    //setCount(count - 1);
   };
 
   const handleChange = (event) => {
     // If the value is ever NaN then just assign a zero.
     const value = parseInt(event.target.value) || 0;
 
-    setValueToAdd(value);
+    //setValueToAdd(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    //setCount(count + valueToAdd);
+    //setValueToAdd(0);
   };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flex flex-row">
         <Button onClick={increment}>
           Increment
@@ -43,7 +63,7 @@ function CounterPage({ initialCount }) {
       <form onSubmit={handleSubmit}>
         <label>Add much more!</label>
         <input
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300" />
